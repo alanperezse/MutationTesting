e6f92f42c855ff7fba7281f42b63357f1aa704b7
Alan Perez <alanperezse@gmail.com>
2022-03-13 10:40:17 -0600

    Initial commit for version control.

---
+++ src/CartesianFormulas.java
@@ -0,0 +1,11 @@
+public class CartesianFormulas {
+    public int Double(Double x1, Double y1, Double x2, Double y2) {
+        return 0;
+    }
+
+    public int slope(int x1, int y1, int x2, int y2) {
+        return (y2 - y1) / (x2 - x1);
+    }
+
+    public int
+}
