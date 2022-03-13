e726b83c15dd6a835b3a64e002561434ac845013
Alan Perez <alanperezse@gmail.com>
2022-03-13 12:25:42 -0600

    Completed implementation and testing. Changed code base theme to Statistics.

---
+++ src/StatisticFormulas.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+import java.util.HashMap; // import the HashMap class
+
+
+public class StatisticFormulas {
+    public static double mean(int[] values) {
+        if(values.length == 0) {
+            return 0;
+        }
+
+        float y = 0;
+
+        // Get cumulative sum
+        for(int value: values) {
+            y += value;
+        }
+
+        // Return average
+        return y / values.length;
+    }
+
+    public static double median(int[] values) throws Exception {
+        // If empty
+        if(values.length == 0) {
+            throw new Exception("Cannot find median of empty array");
+        }
+
+        // Copy to not overwrite input array
+        int[] copy = Arrays.copyOf(values, values.length);
+
+        Arrays.sort(copy);
+
+        if(copy.length % 2 == 0) {    // Even
+            int idx = copy.length / 2;
+            return (float)(copy[idx] + copy[idx - 1]) / 2;
+        } else {                        // Odd
+            int idx = copy.length / 2;
+            return copy[idx];
+        }
+    }
+
+    public static int mode(int[] values) throws Exception {
+        // If empty
+        if(values.length == 0) {
+            throw new Exception("Cannot find mode of empty array");
+        }
+
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+
+        // Count frequencies of nums
+        for(int num: values) {
+            if(frequency.containsKey(num)) {
+                frequency.put(num, frequency.get(num) + 1);
+            } else {
+                frequency.put(num, 1);
+            }
+        }
+
+
+        // Find num with greatest frequency
+        int greatestFrequency = 0;
+        int mode = -1;
+        int numFrequency;
+        for(int num : frequency.keySet()) {
+            numFrequency = frequency.get(num);
+
+            if(numFrequency > greatestFrequency) {
+                greatestFrequency = numFrequency;
+                mode = num;
+            }
+        }
+
+        return mode;
+    }
+}
---
+++ src/StatisticFormulasTest.java
@@ -0,0 +1,114 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticFormulasTest {
+    @Test
+    void mean1() {
+        int[] values = {0, 1, 2, 3, 4};
+        double expected = 2;
+        double actual = StatisticFormulas.mean(values);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void mean2() {
+        int[] values = {};
+        double expected = 0;
+        double actual = StatisticFormulas.mean(values);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void mean3() {
+        int[] values = {3, 6, 9, 10, 100};
+        double expected = 25.6f;
+        double actual = StatisticFormulas.mean(values);
+        assertEquals(expected, actual);
+    }
+
+    // Should return middle value 2
+    @Test
+    void median1() {
+        int[] values = {2, 1, 0, 4, 3};
+        double expected = 2;
+
+        try {
+            double actual = StatisticFormulas.median(values);
+            assertEquals(expected, actual);
+        } catch (Exception e) {
+            System.out.println(e);
+            fail();
+        }
+    }
+
+    // Should throw an exception
+    @Test
+    void median2() {
+        int[] values = {};
+
+        try {
+            StatisticFormulas.median(values);
+            fail();
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+
+    // Should return average of middle values
+    @Test
+    void median3() {
+        int[] values = {-1, 0, -100, 50, 33, 21};
+        double expected = 10.5;
+
+        try {
+            double actual = StatisticFormulas.median(values);
+            assertEquals(expected, actual);
+        } catch (Exception e) {
+            System.out.println(e);
+            fail();
+        }
+    }
+
+    // Return first most frequent value found 0
+    @Test
+    void mode1() {
+        int[] values = {0, 1, 2, 3, 4, 5};
+        double expected = 0;
+
+        try {
+            double actual = StatisticFormulas.mode(values);
+            assertEquals(expected, actual);
+        } catch (Exception e) {
+            System.out.println(e);
+            fail();
+        }
+    }
+
+    // Throws exception
+    @Test
+    void mode2() {
+        int[] values = {};
+
+        try {
+            StatisticFormulas.mode(values);
+            fail();
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+    }
+
+    // Returns most frequent value 4
+    @Test
+    void mode3() {
+        int[] values = {5, 3, 4, -1, 4 -1, 5, 5, 5, 4, 100, 4, 5, 4, 8, 4, 4};
+        double expected = 4;
+
+        try {
+            double actual = StatisticFormulas.mode(values);
+            assertEquals(expected, actual);
+        } catch (Exception e) {
+            System.out.println(e);
+            fail();
+        }
+    }
+}
